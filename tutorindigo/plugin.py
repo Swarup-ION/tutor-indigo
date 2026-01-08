cbda6a8ab515388828eb810f8c7a964e6a5105b8
Sahil Gupta <sahil9013950082@gmail.com>
2026-01-09 03:05:28 +0530

    Update plugin.py

--- tutorindigo/plugin.py
+++ tutorindigo/plugin.py
@@ -114,7 +114,7 @@ for mfe in indigo_styled_mfes:
                 """
 RUN npm install @edly-io/indigo-frontend-component-footer@^3.0.0
 RUN npm install '@edx/frontend-component-header@npm:@edly-io/indigo-frontend-component-header@^4.0.0'
-RUN npm install '@edx/brand@git+https://github.com/Swarup-ION/brand-openedx#938bdda'
+RUN npm install '@edx/brand@git+https://github.com/Swarup-ION/brand-openedx#d6fe7ce'
 
 """,  # noqa: E501
             ),
@@ -131,7 +131,7 @@ const { default: IndigoFooter } = await import('@edly-io/indigo-frontend-compone
 hooks.Filters.ENV_PATCHES.add_item(
     (
         "mfe-dockerfile-post-npm-install-authn",
-        "RUN npm install '@edx/brand@git+https://github.com/Swarup-ION/brand-openedx#938bdda'",
+        "RUN npm install '@edx/brand@git+https://github.com/Swarup-ION/brand-openedx#d6fe7ce'",
     )
 )
 
